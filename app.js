a14e030e6ec9853c36581998b8a3f2e970764b31
ctaar16 <69216405+ctaar16@users.noreply.github.com>
2020-09-22 10:22:42 -0600

    Tuesday

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
-// API docs: https://openweathermap.org/current
+//API docs: https://openweathermap.org/current
 //API Key: 3d555794e8bf23873faadec90a413459
 
 
-// const apiKey = '3d555794e8bf23873faadec90a413459'
+//vars/const
 
-// async function getWeather(city) {
-//     try {
-//         const url = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
-//        const response = await axios.get(url)
-//        console.log(response.data) 
-//     } catch (error) {
-//         console.log(`Error: ${error}`)
-//     }
-// }
+ const containerElement = document.querySelector(".weather-container");
+ const iconElement = document.querySelector(".weather-icon");
+ const tempElement = document.querySelector(".temp-value p");
+ const locationElement = document.querySelector(".location p");
+ const descripElement = document.querySelector('.temp-descrip')
 
-// getWeather('Chicago')
+
+const apiKey = '3d555794e8bf23873faadec90a413459';
+const weatherData = {};
 
 
 
-//**************** 2nd SnowData API Below */
-
-
-
-const apiKey = '3d555794e8bf23873faadec90a413459'
-
-async function getWeather(city) {
+async function grabWeather(city) {
     try {
-        const url = `https://cors-anywhere.herokuapp.com/http://api.powderlin.es/station/791:WA:SNTL?startdate=2013-01-15&enddate=2013-01-15`
-       const response = await axios.get(url)
-       console.log(response.data) 
+        const url = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+        const response = await axios.get(url)
+        console.log(response.data) 
+
     } catch (error) {
         console.log(`Error: ${error}`)
     }
 }
 
-getWeather('Keystone')
+grabWeather("denver")
+
+
+getWeather(function(data) {
+    console.log(data.list[0].weather[0].description)
+})
