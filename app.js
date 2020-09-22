6758029792e05c90b3fa2f72a337af8f2b404027
ctaar16 <69216405+ctaar16@users.noreply.github.com>
2020-09-22 12:42:54 -0600

    newcommit

--- app.js
+++ app.js
@@ -2,17 +2,10 @@
 //API Key: 3d555794e8bf23873faadec90a413459
 
 
-//vars/const
-
- const containerElement = document.querySelector(".weather-container");
- const iconElement = document.querySelector(".weather-icon");
- const tempElement = document.querySelector(".temp-value p");
- const locationElement = document.querySelector(".location p");
- const descripElement = document.querySelector('.temp-descrip')
-
-
 const apiKey = '3d555794e8bf23873faadec90a413459';
 const weatherData = {};
+
+
 
 
 
@@ -26,10 +19,89 @@ async function grabWeather(city) {
         console.log(`Error: ${error}`)
     }
 }
+grabWeather("aurora")
 
-grabWeather("denver")
+// weather container :
+
+const weatherContainer = weather => {
+   
+   
+ //temp
+const temperature = document.createElement('p')
+let temp = response.data.main.temp
+temperature.innerHTML = `temp: ${temp}`
+document.querySelector('.temp-value').append(`temp: ${temp}`)
+
+//weather icon
+const icon = document.createElement('p')
+let pic = response.data.weather[3]
+icon = innerHTML = `pic: ${pic}`
+document.querySelector('.weather-icon').append(`pic: ${pic}`)
+
+//location/city
+const location = document.createElement('p')
+let place = response.data.name
+location.innerHTML = `place: ${place}`
+document.querySelector('.location').append(`place: ${place}`)
+
+//weather description
+const description = document.createElement('p')
+let detail = response.data.weather[2]
+description.innerHTML = `detail: ${detail}`
+document.querySelector('.weather-descrip').append(`detail: ${detail}`)
+}
 
 
-getWeather(function(data) {
-    console.log(data.list[0].weather[0].description)
-})
+
+
+
+
+// //temp
+// const temperature = document.createElement('p')
+// let temp = response.data.main.temp
+// temperature.innerHTML = `temp: ${temp}`
+// document.querySelector('.temp-value').append(`temp: ${temp}`)
+
+// //weather icon
+// const icon = document.createElement('p')
+// let pic = response.data.weather[3]
+// icon = innerHTML = `pic: ${pic}`
+// document.querySelector('.weather-icon').append(`pic: ${pic}`)
+
+// //location/city
+// const location = document.createElement('p')
+// let place = response.data.name
+// location.innerHTML = `place: ${place}`
+// document.querySelector('.location').append(`place: ${place}`)
+
+// //weather description
+
+// const description = document.createElement('p')
+// let detail = response.data.weather[2]
+// description.innerHTML = `detail: ${detail}`
+// document.querySelector('.weather-descrip').append(`detail: ${detail}`)
+
+
+// //Button
+
+    const button = document.createElement('p')
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Celsius  to Fahrenheit conversion
+// function fahrenheit(temperature){
+//     return (temperature * 9/5) + 32;
+// }
+
+
+
